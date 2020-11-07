2b14217ed55b303873c012e412be8c47ba917d0c
tomataso <tomataso@users.noreply.github.com>
2020-11-07 15:47:31 -0600

    Hasta el 17

---
+++ journal/src/main/java/com/cenfotec/journal/service/JournalServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cenfotec.journal.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cenfotec.journal.domain.Journal;
+import com.cenfotec.journal.repository.JournalRepository;
+
+public class JournalServiceImpl implements JournalService {
+@Autowired
+JournalRepository journalRepo;
+@Override
+public void saveJournal(Journal newJournal) {
+journalRepo.save(newJournal);
+}
+@Override
+public List<Journal> getAllJournals() {
+return journalRepo.findAll();
+}
+}
